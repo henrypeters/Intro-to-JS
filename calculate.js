3f7ee84843a6e99d9f03834f6f4b03873dc8d587
henrypeters <meethenry09@gmail.com>
2022-09-03 11:16:31 +0100

    feat: implement showName  logic

---
+++ calculate.js
@@ -0,0 +1,8 @@
+function calculateNum(num1, num2) {
+	return num1 + num2
+}
+
+
+function getNum(x, y) {
+	console.log(calculateNum(x, y))
+}
--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,4 @@
-~function sayName(name) {
-	console.log(`My name is ${name}`)
-}
+
 
 function calculateNum(num1, num2) {
 	return num1 + num2
@@ -9,5 +7,16 @@ function calculateNum(num1, num2) {
 
 function getNum(x, y) {
 	console.log(calculateNum(x, y))
+}
+
+
+function showName() {
+	const showNameElement = document.getElementById('showName')
+	let name = document.getElementById('nameInput')
+	name = name.value
+	// sayName(name)
+	showNameElement.style.display = 'block'
+	showNameElement.style.color = 'purple'
+	showNameElement.innerHTML = name
 }
 
